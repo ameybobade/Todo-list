aa83aa4cd628cea1f8784d15cf9ec75adf5b9b40
ameybobade <ameybobade@gmail.com>
2021-06-10 16:13:12 +0530

    css changed

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export const About = () => {
+    let myStyles={
+        minHeight:"85vh",
+    }
     let hStyle={
         minHeight: "100%",
         fontSize:"30px",
@@ -11,8 +14,11 @@ export const About = () => {
     }
 
     return (
-        <div className="container my-4">
+        <div className="container my-4" style={myStyles}>
             <h2 style={hStyle} className="text-center">About Us</h2>
+            <p style={paraStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum perferendis eaque excepturi officia voluptatum omnis nisi deserunt recusandae laudantium exercitationem repellendus vero praesentium provident voluptas aspernatur fuga explicabo voluptates illum laboriosam quo eius, ex blanditiis dolorum! Dolore, saepe! Adipisci, magnam.</p>
+            <p style={paraStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum perferendis eaque excepturi officia voluptatum omnis nisi deserunt recusandae laudantium exercitationem repellendus vero praesentium provident voluptas aspernatur fuga explicabo voluptates illum laboriosam quo eius, ex blanditiis dolorum! Dolore, saepe! Adipisci, magnam.</p>
+            <p style={paraStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum perferendis eaque excepturi officia voluptatum omnis nisi deserunt recusandae laudantium exercitationem repellendus vero praesentium provident voluptas aspernatur fuga explicabo voluptates illum laboriosam quo eius, ex blanditiis dolorum! Dolore, saepe! Adipisci, magnam.</p>
             <p style={paraStyle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum perferendis eaque excepturi officia voluptatum omnis nisi deserunt recusandae laudantium exercitationem repellendus vero praesentium provident voluptas aspernatur fuga explicabo voluptates illum laboriosam quo eius, ex blanditiis dolorum! Dolore, saepe! Adipisci, magnam.</p>
         </div>
     )
